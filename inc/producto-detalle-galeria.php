f84d8bd4449705769f499ea1dfa40243543addc7
GerardoMac <centrodph@gmail.com>
2015-09-08 08:03:22 -0300

    Trasicion Fix

--- inc/producto-detalle-galeria.php
+++ inc/producto-detalle-galeria.php
@@ -1,39 +1,11 @@
 <script>
     jQuery(document).ready(function ($) {
         var _SlideshowTransitions = [
-            //Zoom- in
-            {$Duration: 1200, $Zoom: 1, $Easing: {$Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad}, $Opacity: 2},
-            //Zoom+ out
-            {$Duration: 1000, $Zoom: 11, $SlideOut: true, $Easing: {$Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear}, $Opacity: 2},
-            //Rotate Zoom- in
-            {$Duration: 1200, $Zoom: 1, $Rotate: 1, $During: {$Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]}, $Easing: {$Zoom: $JssorEasing$.$EaseSwing, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseSwing}, $Opacity: 2, $Round: {$Rotate: 0.5}},
-            //Rotate Zoom+ out
-            {$Duration: 1000, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Easing: {$Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo}, $Opacity: 2, $Round: {$Rotate: 0.8}},
-            //Zoom HDouble- in
-            {$Duration: 1200, x: 0.5, $Cols: 2, $Zoom: 1, $Assembly: 2049, $ChessMode: {$Column: 15}, $Easing: {$Left: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear}, $Opacity: 2},
-            //Zoom HDouble+ out
-            {$Duration: 1200, x: 4, $Cols: 2, $Zoom: 11, $SlideOut: true, $Assembly: 2049, $ChessMode: {$Column: 15}, $Easing: {$Left: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear}, $Opacity: 2},
-            //Rotate Zoom- in L
-            {$Duration: 1200, x: 0.6, $Zoom: 1, $Rotate: 1, $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]}, $Easing: {$Left: $JssorEasing$.$EaseSwing, $Zoom: $JssorEasing$.$EaseSwing, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseSwing}, $Opacity: 2, $Round: {$Rotate: 0.5}},
-            //Rotate Zoom+ out R
-            {$Duration: 1000, x: -4, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Easing: {$Left: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo}, $Opacity: 2, $Round: {$Rotate: 0.8}},
-            //Rotate Zoom- in R
-            {$Duration: 1200, x: -0.6, $Zoom: 1, $Rotate: 1, $During: {$Left: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8]}, $Easing: {$Left: $JssorEasing$.$EaseSwing, $Zoom: $JssorEasing$.$EaseSwing, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseSwing}, $Opacity: 2, $Round: {$Rotate: 0.5}},
-            //Rotate Zoom+ out L
-            {$Duration: 1000, x: 4, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Easing: {$Left: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo}, $Opacity: 2, $Round: {$Rotate: 0.8}},
-            //Rotate HDouble- in
-            {$Duration: 1200, x: 0.5, y: 0.3, $Cols: 2, $Zoom: 1, $Rotate: 1, $Assembly: 2049, $ChessMode: {$Column: 15}, $Easing: {$Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad, $Rotate: $JssorEasing$.$EaseInCubic}, $Opacity: 2, $Round: {$Rotate: 0.7}},
-            //Rotate HDouble- out
-            {$Duration: 1000, x: 0.5, y: 0.3, $Cols: 2, $Zoom: 1, $Rotate: 1, $SlideOut: true, $Assembly: 2049, $ChessMode: {$Column: 15}, $Easing: {$Left: $JssorEasing$.$EaseInExpo, $Top: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo}, $Opacity: 2, $Round: {$Rotate: 0.7}},
-            //Rotate VFork in
-            {$Duration: 1200, x: -4, y: 2, $Rows: 2, $Zoom: 11, $Rotate: 1, $Assembly: 2049, $ChessMode: {$Row: 28}, $Easing: {$Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad, $Rotate: $JssorEasing$.$EaseInCubic}, $Opacity: 2, $Round: {$Rotate: 0.7}},
-            //Rotate HFork in
-            {$Duration: 1200, x: 1, y: 2, $Cols: 2, $Zoom: 11, $Rotate: 1, $Assembly: 2049, $ChessMode: {$Column: 19}, $Easing: {$Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad, $Rotate: $JssorEasing$.$EaseInCubic}, $Opacity: 2, $Round: {$Rotate: 0.8}}
+            {$Duration: 1200, y: -4, $Zoom: 11, $Easing: {$Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseOutQuad}, $Opacity: 2}
         ];
-
         var options = {
-            $AutoPlay: true, 
-            $AutoPlayInterval: 1500, 
+            $AutoPlay: true,
+            $AutoPlayInterval: 1500,
             $PauseOnHover: 1,
             $DragOrientation: 3, //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
             $ArrowKeyNavigation: true, //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
@@ -82,11 +54,7 @@
         //responsive code end
     });
 </script>
-<!-- Jssor Slider Begin -->
-<!-- To move inline styles to css file/block, please specify a class name for each element. --> 
 <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 740px; height: 550px; overflow: hidden;">
-
-    <!-- Slides Container -->
     <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 580px; height: 550px; overflow: hidden;">
         <div>
             <img u="image" src="images/galeria/img1.png" />
--- inc/producto-detalle.php
+++ inc/producto-detalle.php
@@ -1,21 +1,25 @@
-<div class="producto-detalle-content">
-    <div class="producto-detalle-top">
-        <div class="row">
-            <div class="col-md-7">
-                <div class="producto-detalle-galeria">
-                    <?php include('inc/producto-detalle-galeria.php');?>
-                </div>
-            </div>
-            <div class="dol-md-5">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="producto-detalle-content">
                 <div class="producto-detalle-top">
-                    <?php include('inc/producto-detalle-top.php');?>
-                </div>    
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div class="producto-detalle-galeria">
+                                <?php include('inc/producto-detalle-galeria.php'); ?>
+                            </div>
+                        </div>
+                        <div class="dol-md-5">
+                            <div class="producto-detalle-top">
+                                <?php include('inc/producto-detalle-top.php'); ?>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="producto-detalle-bottom">
+                    <?php include('inc/producto-detalle-tabs.php'); ?>
+                </div>
             </div>
         </div>
     </div>
-    <div class="producto-detalle-bottom">
-        <?php include('inc/producto-detalle-tabs.php');?>
-    </div>
-    
-    
 </div>
